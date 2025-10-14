9cfb13e3248f23dd60eee0766b85c1d6b0b9c7bf
Mosho <moises.leon@chasacademy.se>
2025-10-14 19:56:09 +0200

    övningar av vite

--- mos-test/ovningar/timer.js
+++ mos-test/ovningar/timer.js
@@ -4,7 +4,7 @@ import confetti from "canvas-confetti"
 const btn = document.getElementById("btn")
 const timer = document.getElementById("timer")
 
-btn.onclick = () => {
+/* btn.onclick = () => {
     const timer = setInterval(() => {
         confetti()
     },1000)
@@ -12,4 +12,27 @@ btn.onclick = () => {
     setTimeout (() => {
         clearInterval(timer)
     },1000)
+} */
+
+btn.onclick = () => {
+    let timeleft = 5
+    
+    const countDown = () => {
+        timer.textContent = timeleft
+        if (timeleft > 0) {
+            timeleft--
+            setTimeout(countDown, 1000)
+        }else {
+            timer.textContent = "Kabom"
+            confetti()
+        }
+    }
+
+   countDown()
 }
+
+/* btn.onclick = () => {
+    setTimeout(() => {
+        confetti()
+    }, 1000) // kör confetti efter 1 sekund
+} */
