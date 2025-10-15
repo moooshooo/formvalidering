2067512c7c1d08b0cf35ce73ddb4b9de26c5ee47
Mosho <moises.leon@chasacademy.se>
2025-10-15 07:56:12 +0200

    Sweetalert2

---
+++ mos-test/ovningar/sweetalert2.js
@@ -0,0 +1,14 @@
+import swal from "sweetalert2"
+
+const tryMe = document.getElementById("tryMe")
+const 
+
+tryMe.onclick = () => {
+    
+    if
+    swal.fire({
+    title: "Fan va bra!",
+    text: "Du kunde inte hålla dig!",
+    icon: "success"
+});
+}
